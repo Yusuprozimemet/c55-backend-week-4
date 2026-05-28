a807f2a796e94f7d90c555973b4e935e8cc5d349
Yusuprozimemet <yusuf.rozimemet@gmail.com>
2026-05-28 19:37:00 +0200

    api layer works well with create, retrieve records

--- task-1/src/main/java/com/analyticsapi/week4/controller/AnalyticsController.java
+++ task-1/src/main/java/com/analyticsapi/week4/controller/AnalyticsController.java
@@ -1,4 +1,37 @@
 package com.analyticsapi.week4.controller;
 
+import com.analyticsapi.week4.dto.AnalyticsRecordCreateRequest;
+import com.analyticsapi.week4.model.AnalyticsRecord;
+import com.analyticsapi.week4.service.AnalyticsService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+
+@RestController
+@RequestMapping("/api/analytics-records")
 public class AnalyticsController {
+
+    private final AnalyticsService service;
+
+    public AnalyticsController(AnalyticsService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<AnalyticsRecord> create(
+            @RequestBody @Valid AnalyticsRecordCreateRequest record) {
+        AnalyticsRecord created = service.create(record);
+        URI location = URI.create("/api/analytics-records/" + created.getId());
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AnalyticsRecord> getById(@PathVariable String id) {
+        AnalyticsRecord record = service.getById(id);
+        return ResponseEntity.ok(record);
+    }
 }
